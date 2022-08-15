e9244493cef46253e2d34f8a903d68c7c8d31c57
Dimitreee <danewdima@gmail.com>
2022-08-15 16:58:52 +0300

    FIR-14: fix pm2 config, fix ecosystem js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     apps : [{
         name   : "perp",
-        script : "./start.sh"
+        script : "./start.sh",
+        env_production: {
+            L2_WEB3_ENDPOINTS: "https://mainnet.infura.io/v3/8f8bfe57ab9e4ced96813ab9896a2f36"
+        },
+        env_development: {
+            L2_WEB3_ENDPOINTS: "https://mainnet.infura.io/v3/8f8bfe57ab9e4ced96813ab9896a2f36"
+        },
     }]
 }
