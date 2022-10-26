778e4eb2763bf12609dd5a6b392f623b562317b4
Aleksandr Kolod'ko <cm-brain@yandex.ru>
2022-10-26 12:09:31 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,10 @@ module.exports = {
         name   : "perp",
         script : "./start.sh",
         env_production: {
-            L2_WEB3_ENDPOINTS: "https://optimism-mainnet.infura.io/v3/8f8bfe57ab9e4ced96813ab9896a2f3"
+            L2_WEB3_ENDPOINTS: "https://optimism-mainnet.infura.io/v3/8f8bfe57ab9e4ced96813ab9896a2f36"
         },
         env_development: {
-            L2_WEB3_ENDPOINTS: "https://optimism-mainnet.infura.io/v3/8f8bfe57ab9e4ced96813ab9896a2f3"
+            L2_WEB3_ENDPOINTS: "https://optimism-mainnet.infura.io/v3/8f8bfe57ab9e4ced96813ab9896a2f36"
         },
     }]
 }
